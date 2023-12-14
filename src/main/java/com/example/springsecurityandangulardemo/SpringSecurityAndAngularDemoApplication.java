bc7cb1683ae41f61ea32f25c21997fa47cb6228c
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-12-14 10:42:36 -0600

    Initial commit

---
+++ src/main/java/com/example/springsecurityandangulardemo/SpringSecurityAndAngularDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityandangulardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAndAngularDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAndAngularDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityandangulardemo/SpringSecurityAndAngularDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityandangulardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAndAngularDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
